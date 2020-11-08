3fef59e5a7504beebf5f584c3df1cfd00a89fb5a
ChenZhaoheng-HKU <71821616+ChenZhaoheng-HKU@users.noreply.github.com>
2020-11-08 20:31:23 +0800

    Update init_game.cpp

--- init_game.cpp
+++ init_game.cpp
@@ -10,7 +10,7 @@ void game_ini (Game game, Player player[12]){
 	srand((int)time(0));
 	int idx[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
 	random_shuffle(begin(idx), end(idx));
-	for (int i=1; i <= 11; i++){
+	for (int i=0; i <= 11; i++){
     		player[i].player_index = i;
     		player[i].player_role = idx[i];
     	        switch (idx[i]) {
