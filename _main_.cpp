26f5de878a9df41ce2c452d6e9ff9bf2ab6b6f7f
Rua76 <71935749+Rua76@users.noreply.github.com>
2020-11-08 17:42:56 +0800

    Update _main_.cpp

--- _main_.cpp
+++ _main_.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+void Role_distribute (){
+    
+}
+
+struct game {
+    int player_num;
+    int turn;
+    
+};
+game Game;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "select the number of players: ";
+    cin >> Game.player_num;
+    return 0;
+}
