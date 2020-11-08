fd625b7c6f25359671de0f860bd4003e2cb679b4
ChenZhaoheng-HKU <71821616+ChenZhaoheng-HKU@users.noreply.github.com>
2020-11-08 19:36:28 +0800

    Update init_game.cpp

--- init_game.cpp
+++ init_game.cpp
@@ -1,54 +1,19 @@
 #include <iostream>
 #include "init_game.h"
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
 using namespace std;
+
 void game_ini (Game game, Player player[13]){
-    // choose player number
-    cout << "select the number of players between 9-12: ";
-    cin >> game.player_num;
-    //initialize players
-    switch (game.player_num) {
-        case 9:
-            game.wolf_num = 3;
-            game.good_num = 6;
-            game.villager_num = 3;
-            game.seer_num = 1;
-            game.guard_num = 1;
-            game.witch_num = 1;
-            break;
-        case 10:
-            game.wolf_num = 3;
-            game.good_num = 7;
-            game.villager_num = 4;
-            game.seer_num = 1;
-            game.guard_num = 1;
-            game.witch_num = 1;
-            break;
-        case 11:
-            game.wolf_num = 3;
-            game.good_num = 8;
-            game.villager_num = 4;
-            game.seer_num = 1;
-            game.guard_num = 1;
-            game.witch_num = 1;
-            game.hunter_num = 1;
-            break;
-        case 12:
-            game.wolf_num = 4;
-            game.good_num = 8;
-            game.villager_num = 4;
-            game.seer_num = 1;
-            game.guard_num = 1;
-            game.witch_num = 1;
-            game.hunter_num = 1;
-            break;
-        default:
-            cout << "Invalid number!" << endl;
-    }
-    //player initialize
-for (int i=1; i <= 12; i++){
-    	player[i].player_index = i;
-    	player[i].player_role = idx[i];
-    	    switch (idx[i]) {
+
+	srand((int)time(0));
+	int idx[12] = {1,2,3,4,5,6,7,8,9,10,11,12};
+	random_shuffle(begin(idx), end(idx));
+	for (int i=1; i <= 12; i++){
+    		player[i].player_index = i;
+    		player[i].player_role = idx[i];
+    	        switch (idx[i]) {
       		case 1:
       		case 2:
       		case 3:
