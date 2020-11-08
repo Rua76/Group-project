7005715498937a9d34486ecdb0fe7a347f57441e
ChenZhaoheng-HKU <71821616+ChenZhaoheng-HKU@users.noreply.github.com>
2020-11-08 19:28:06 +0800

    Update init_game.cpp

--- init_game.cpp
+++ init_game.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include "init_game.h"
 using namespace std;
-
-void game_ini (Game game, Player player){
+void game_ini (Game game, Player player[13]){
     // choose player number
     cout << "select the number of players between 9-12: ";
     cin >> game.player_num;
@@ -45,10 +44,26 @@ void game_ini (Game game, Player player){
         default:
             cout << "Invalid number!" << endl;
     }
-    //player index
-    for (int i = 0; i < game.player_num; i++){
-        player.player_index[i] = i;
-    }
-    //roles
+    //player initialize
+for (int i=1; i <= 12; i++){
+    	player[i].player_index = i;
+    	player[i].player_role = idx[i];
+    	    switch (idx[i]) {
+      		case 1:
+      		case 2:
+      		case 3:
+			case 4: { player[i].role = "Villager" ;break;}
+      		case 5:
+      		case 6:
+      		case 7: 
+			case 8: { player[i].role = "Werewolf" ;break;}
+      		case 9: { player[i].role = "Seer";break;}
+      		case 10: { player[i].role = "Guard";break; }
+      		case 11: { player[i].role = "Witch" ;break;}
+      		case 12: { player[i].role = "Hunter" ;break; }
+    		}//switch
+    		cout << "Player " <<  player[i].player_index << " is " << player[i].role << endl;//运行可删
+    
+	}
 }
 
