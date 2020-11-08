1610f77cf59314ca8013c7176af263029871b520
Rua76 <71935749+Rua76@users.noreply.github.com>
2020-11-08 18:58:37 +0800

    Update init_game.h

--- init_game.h
+++ init_game.h
@@ -1,4 +1,4 @@
-class Game{
+struct Game {
     int player_num;
     int turn = 1;
     int wolf_num;
@@ -8,11 +8,11 @@ class Game{
     int witch_num;
     int guard_num;
     int hunter_num;
-  public:
-    void game_ini ();
 };
 
-class Player{
+struct Player{
     int player_index[20];
     string role[20];
 };
+
+void game_ini (Game game, Player player);
